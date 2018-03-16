1f9850963cf5a381afe8e5c3a2f62b933a386874
trucsphuongw <osxvn@trucphuong.local>
2018-03-16 18:10:46 +0700

    Initial Commit

---
+++ WrokingWithCameraAlbum/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WrokingWithCameraAlbum
+//
+//  Created by Trúc Phương >_< on 16/03/2018.
+//  Copyright © 2018 iDev Bao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
